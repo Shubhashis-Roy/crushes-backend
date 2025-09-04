479ced946030ff7bbe326c0b63b99b5e00792526
Shubhashis Roy <shubhashisroy360@gmail.com>
2025-09-04 19:22:58 +0530

    feat: user list api

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
 const { userAuth } = require("../middlewares/auth");
-const { chatting } = require("../controller/chatController");
+const { chatting, getChatUsersList } = require("../controller/chatController");
 
 const chatRouter = express.Router();
+
+chatRouter.get("/chat/users-list", userAuth, getChatUsersList);
 
 chatRouter.get("/chat/:targetUserId", userAuth, chatting);
 
